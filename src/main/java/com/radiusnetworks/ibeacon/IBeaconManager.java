74012b5247a89da75537bef5a52d3ca529c41bd4
David G. Young <dyoung@Davids-MacBook-Pro-2.local>
2014-01-30 09:18:54 -0500

    Fix setting foreground scan period per https://github.com/RadiusNetworks/android-ibeacon-service/issues/17

--- src/main/java/com/radiusnetworks/ibeacon/IBeaconManager.java
+++ src/main/java/com/radiusnetworks/ibeacon/IBeaconManager.java
@@ -134,9 +134,7 @@ public class IBeaconManager {
      * Sets the duration in milliseconds of each Bluetooth LE scan cycle to look for iBeacons
      * @param p
      */
-    public void setForegroundScanPeriod(long p) {
-        foregroundBetweenScanPeriod = p;
-    }
+    public void setForegroundScanPeriod(long p) { foregroundScanPeriod = p; }
     /**
      * Sets the duration in milliseconds to wait between each bluetooth scan cycle used to look for iBeacons
      * @param p

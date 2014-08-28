a3a18fc54bcf3452342db3f8b35e9545ef87e24d
David G. Young <david@radiusnetworks.com>
2014-08-28 17:11:23 -0400

    remove model-speicfic distance calcs from master

--- src/main/java/org/altbeacon/beacon/distance/DistanceCalculator.java
+++
@@ -1,8 +0,0 @@
-package org.altbeacon.beacon.distance;
-
-/**
- * Created by dyoung on 8/28/14.
- */
-public interface DistanceCalculator {
-    public double calculateDistance(int txPower, double rssi);
-}

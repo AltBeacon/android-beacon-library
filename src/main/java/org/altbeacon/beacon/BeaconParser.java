3caa7c31676c57db7394b5b7b9580a4f3ca289b0
David G. Young <david@radiusnetworks.com>
2014-10-09 18:18:47 -0400

    correct : to be = in example expression

--- src/main/java/org/altbeacon/beacon/BeaconParser.java
+++ src/main/java/org/altbeacon/beacon/BeaconParser.java
@@ -21,7 +21,7 @@ import java.util.regex.Pattern;
  *
  * <pre><code>
  * BeaconManager.getBeaconParsers().add(new BeaconParser()
- *   .setBeaconLayout("m:2-3:beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+ *   .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
  * </pre></code>
  *
  * <p>

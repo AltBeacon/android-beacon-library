16dfacf8c100d5ba0595694b828b77018266616a
Sam Duke <samskiter@users.noreply.github.com>
2015-03-31 17:58:59 +0100

    Always deliver scan/ranging results on the main thread

--- src/main/java/org/altbeacon/beacon/BeaconIntentProcessor.java
+++ src/main/java/org/altbeacon/beacon/BeaconIntentProcessor.java
@@ -44,6 +44,19 @@ public class BeaconIntentProcessor extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
+        //Make sure we always deliver results on the main thread
+        if (Looper.getMainLooper().getThread() != Thread.currentThread()) {
+            Handler mainHandler = new Handler(Looper.getMainLooper());
+        
+            Runnable myRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    onHandleIntent(intent)
+                }
+            };
+            mainHandler.post(myRunnable);
+            return;
+        }
 		LogManager.d(TAG, "got an intent to process");
 		
 		MonitoringData monitoringData = null;

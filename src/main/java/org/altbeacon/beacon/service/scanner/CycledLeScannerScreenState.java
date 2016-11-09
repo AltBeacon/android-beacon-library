c694242e8aa3799a8ae7bc6e596d1cae47a9e4c9
olivier stevens <olivier.stevens@connecthings.com>
2016-11-09 09:50:02 +0100

    Ready to be merged

--- src/main/java/org/altbeacon/beacon/service/scanner/CycledLeScannerScreenState.java
+++ src/main/java/org/altbeacon/beacon/service/scanner/CycledLeScannerScreenState.java
@@ -1,6 +1,8 @@
 package org.altbeacon.beacon.service.scanner;
 
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.logging.LogManager;
+import org.altbeacon.beacon.logging.Logger;
 import org.altbeacon.beacon.service.scanner.screenstate.ScreenStateListener;
 
 /**
@@ -9,6 +11,7 @@ import org.altbeacon.beacon.service.scanner.screenstate.ScreenStateListener;
 
 public class CycledLeScannerScreenState extends CycledLeScanner implements ScreenStateListener {
 
+    private static final String TAG = "CycledLeScannerScreenState";
 
     public static enum SCAN_ON_SCREEN_STATE {ON_ONLY(false, true), OFF_ONLY(true, false), ON_OFF(true, true);
 
@@ -27,7 +30,7 @@ public class CycledLeScannerScreenState extends CycledLeScanner implements Scree
         }
     };
 
-    private boolean mScanIsEnabled;
+    private boolean scanIsEnabled;
     private int mActiveScanningTimeOnScreenSwitchState;
     private SCAN_ON_SCREEN_STATE mScanScreenState;
 
@@ -41,52 +44,63 @@ public class CycledLeScannerScreenState extends CycledLeScanner implements Scree
 
     public CycledLeScannerScreenState(long scanPeriod, long betweenScanPeriod, boolean backgroundFlag, int activeScanningTimeOnScreenSwitchState, SCAN_ON_SCREEN_STATE scanScreenState) {
         super(scanPeriod, betweenScanPeriod, backgroundFlag);
-        mScanIsEnabled = false;
+        scanIsEnabled = false;
         mActiveScanningTimeOnScreenSwitchState = activeScanningTimeOnScreenSwitchState;
         mScanScreenState = scanScreenState;
     }
 
     protected long calculateNextTimeToStartScanInBg(){
-        if(mScanIsEnabled){
+        if(scanIsEnabled){
             return super.calculateNextTimeToStartScanInBg();
         }
         return 1000;
     }
 
     protected long calculateNextTimeForScanCycleStopInBg(){
-        if(mScanIsEnabled){
+        if(scanIsEnabled){
             return super.calculateNextTimeForScanCycleStopInBg();
         }
         return 1000;
     }
 
     public synchronized void stopScanningOnSwitchScreenState(){
-        mScanIsEnabled = false;
+        LogManager.d(TAG, "stopScanningOnSwitchScreenState");
+        scanIsEnabled = false;
     }
 
     public synchronized void startScanningOnSwitchScreenState(){
         if(isBackgroundFlag()) {
-            if (mScanIsEnabled) {
+            if (scanIsEnabled) {
+                LogManager.d(TAG, "startScanningOnSwitchScreenState - already in progress - delay the end");
                 cancelRunnable(runnableStopScanning);
             } else {
-                mScanIsEnabled = true;
+                LogManager.d(TAG, "startScanningOnSwitchScreenState - launch the scan");
+                scanIsEnabled = true;
                 setScanPeriods(BeaconManager.DEFAULT_FOREGROUND_SCAN_PERIOD, BeaconManager.DEFAULT_FOREGROUND_BETWEEN_SCAN_PERIOD, isBackgroundFlag(), true);
             }
             postDelayed(runnableStopScanning, mActiveScanningTimeOnScreenSwitchState);
+        }else{
+            LogManager.d(TAG, "startScanningOnSwitchScreenState - app is in foreground - it does not start the scan");
         }
     }
 
     @Override
     public void onScreenOn() {
         if(mScanScreenState.scanWhenScreenOn){
+            LogManager.d(TAG, "onScreenOn - try to launch scanning");
             startScanningOnSwitchScreenState();
+        }else{
+            LogManager.d(TAG, "onScreenOn - no permission to start scanning");
         }
     }
 
     @Override
     public void onScreenOff() {
         if(mScanScreenState.scanWhenScreenOff){
+            LogManager.d(TAG, "onScreenOff - try to launch scanning");
             startScanningOnSwitchScreenState();
+        }else{
+            LogManager.d(TAG, "onScreenOff - no permission to start scanning");
         }
     }
 
